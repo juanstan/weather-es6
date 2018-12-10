bab7e2af071ca2e977a8a3ccccb43a78a91d887f
j.acevedo@sureflap.co.uk <j.acevedo@sureflap.co.uk>
2018-12-10 05:56:45 +0000

    updating test config file

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,10 @@ module.exports = {
         '<rootDir>/src'
     ],
     'transform': {
-        "^.+\\.(t|j)sx?$": "ts-jest"
+        "^.+\\.(j)sx?$": "ts-jest"
     },
     'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?)$',
     'moduleFileExtensions': [
-        'ts',
-        'tsx',
         'js',
         'jsx',
         'json',
